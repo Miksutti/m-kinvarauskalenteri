45f41cfc093f61c8751511cd55731d4b4d7f443b
Miksutti <miika.pitkaranta@seamk.fi>
2021-10-13 18:03:53 +0300

    koodin siirto gittiin

---
+++ script.js
@@ -0,0 +1,83 @@
+const date = new Date();
+
+function lataaKalenteri(){
+
+    const monthDays = document.querySelector("#paivamaarat");
+
+    const kuunVikaPaiva = new Date(date.getFullYear(),date.getMonth() + 1, 0).getDate();
+
+    const edellinenVikaPaiva = new Date(date.getFullYear(),date.getMonth(),0).getDate();
+
+
+    const ekaPaivaIndex = date.getDay() + 2;
+
+    const vikaPaivaIndex = new Date(date.getFullYear(),date.getMonth() + 1, 0).getDay();
+
+    const tulevatPaivat = 7 - vikaPaivaIndex - 1
+
+    const kuukaudet = [
+        "Tammikuu",
+        "Helmikuu",
+        "Maaliskuu",
+        "Huhtikuu",
+        "Toukokuu",
+        "Kesäkuu",
+        "Heinäkuu",
+        "Elokuu",
+        "Syyskuu",
+        "Lokakuu",
+        "Marraskuu",
+        "Joulukuu",
+    ];
+
+    document.querySelector("#kuukausi").innerHTML = kuukaudet[date.getMonth()];
+
+    let paivat = "";
+
+    for (let x = ekaPaivaIndex; x > 0; x--) {
+        paivat += `<div id="entinen">${edellinenVikaPaiva - x + 1}</div>`;
+    }
+
+    for (let i = 1; i <= kuunVikaPaiva; i++) {
+        if (i === new Date().getDate() &&
+            date.getMonth() === new Date().getMonth()
+        ) {
+            paivat += `<div id="tamapaiva">${i}
+            <p id="varaustanaan"></p>
+            </div>`;
+        } else {
+            paivat += `<div>${i}
+            <p id="varaus${i}"></p>
+            </div>`;
+        }
+    }
+
+    for (let j = 1; j <= tulevatPaivat; j++) {
+        paivat += `<div id="tuleva">${j}</div>`;
+        monthDays.innerHTML = paivat;
+    }
+
+}
+
+document.querySelector("#edellinen").addEventListener("click", () => {
+    date.setMonth(date.getMonth() - 1);
+    lataaKalenteri();
+  });
+  
+document.querySelector("#seuraava").addEventListener("click", () => {
+    date.setMonth(date.getMonth() + 1);
+    lataaKalenteri();
+  });
+  
+lataaKalenteri();
+
+function uusiVaraus() {
+    var varaus
+    varaus = prompt("Kirjoita varaajan nimi:")
+    if (varaus != null) {
+        document.getElementById("varaus").innerHTML =
+        varaus;
+      }
+}
+
+document.getElementById("paivamaarat").addEventListener("click", uusiVaraus);
